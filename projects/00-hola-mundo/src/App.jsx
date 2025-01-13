de3a6051eb187940c7bb966a315bf98c1322498b
fjruiz82 <fjruiz82@gmail.com>
2025-01-13 13:59:43 +0000

    twCards

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,19 +1,82 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
+const users = [
+    {
+        userName: 'dog',
+        name: 'Sir Dog',
+        isFollowing: true
+    },
+    {
+        userName: 'animal',
+        name: 'Sir Monster',
+        isFollowing: false
+    },
+    {
+        userName: 'unicorn',
+        name: 'Sir Unicorn',
+        isFollowing: false
+    },
+    {
+        userName: 'horse',
+        name: 'Sir Fox',
+        isFollowing: true
+    },
+    {
+        name: 'Sir ???',
+        isFollowing: true
+    }
+]
+
 export function App() {
     //le podemos pasar una funci'on como prop
-    const formatUserName = (userName) => `@${userName}`
+    //const formatUserName = (userName) => `@${userName}`
 
     return (
         //<> es lo mismo que <React.Fragment>
         //si paso el voleano sin ningun valor se entiende true
         //si no paso alguna prop, se entiende undefined, (q es falsy)
-        <div className='App'>
+        /*<div className='App'>
             <TwitterFollowCard formatUserName={formatUserName} userName="dog" name="Sir Dog" isFollowing={true} />
             <TwitterFollowCard formatUserName={formatUserName} userName="animal" name="Sir Monster" isFollowing={false} />
             <TwitterFollowCard formatUserName={formatUserName} userName="unicorn" name="Sir Unicorn" isFollowing />
             <TwitterFollowCard formatUserName={formatUserName} userName="horse" name="Sir Fox" />
-        </div>
+        </div>*/
+
+        /////////////////////////////////////////////////////////////////////////
+
+        // <section className='App'>
+        //     <TwitterFollowCard userName="dog">
+        //         Sir Dog Guau Guau
+        //     </TwitterFollowCard>
+
+        //     <TwitterFollowCard isFollowing userName="animal">
+        //         Sir Monster Uhhhh
+        //     </TwitterFollowCard>
+
+        //     <TwitterFollowCard>
+        //         Sir Abc
+        //     </TwitterFollowCard>
+        // </section>
+
+        ///////////////////////////////////////////////////////////////////////////
+
+        <section className='App'>
+            {
+                users.map(user => {
+                    const { userName, name, isFollowing } = user
+                    return (
+                        <TwitterFollowCard
+                            // incluir siempre una key obligatorio en React, si la bbdd tiene un id, mejor eso q el nombre
+                            key={userName}
+                            userName={userName}
+                            initialIsFollowing={isFollowing}
+                        >
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
+        </section>
     )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,18 +1,32 @@
-export function TwitterFollowCard({ formatUserName, userName, name, isFollowing }) {
+import { useState } from 'react'
+
+export function TwitterFollowCard({ children, userName = 'unknown', initialIsFollowing }) {
+
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+
     const imgSrc = `https://unavatar.io/${userName}`
-console.log(isFollowing)
+    const text = isFollowing ? 'Following' : 'Follow'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+    //console.log(isFollowing)
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img className='tw-followCard-avatar' src={imgSrc} alt="El avatar de Dog" />
                 <div className='tw-followCard-info'>
-                    <strong>{name}</strong>
-                    <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
+                    <strong>{children}</strong>
+                    {/* <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span> */}
+                    <span className='tw-followCard-infoUserName'>@{userName}</span>
                 </div>
             </header>
-            <aside className='tw-followCard-header'>
-                <button className='tw-followCard-button'>
-                    Follow
+            <aside>
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className='tw-followCard-text'>{text}</span>
+                    <span className='tw-followCard-stopFollow'>Unfollow</span>
                 </button>
             </aside>
         </article>
